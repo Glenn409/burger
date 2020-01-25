cff57024aa393160d920b91691abd4f58f135913
Chris Glenn <chrisglenn519@yahoo.com>
2020-01-25 10:59:00 -0600

    updated db

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,23 @@
 const mysql = require('mysql')
 
-const connection = mysql.createConnection('mysql://sft36g6rbqbt885w:q5487vtlh86rzso9@wp433upk59nnhpoh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/burgers_db')
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     port:3306,
-//     user:'root',
-//     password:'password',
-//     database:'burgers_db'
-// })
+const connection;
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port:3306,
+        user:'root',
+        password:'password',
+        database:'burgers_db'
+    })
+}
 
 connection.connect(function(err){
     if(err) {
         console.log('failed to connect to DB')
+    } else {
+        console.log("Connected to DB")
     }
-    console.log("Connected to DB")
 })
 module.exports = connection
