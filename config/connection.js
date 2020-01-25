f6950fbeee62aa1336d9e5222d180386acd659a5
Chris Glenn <chrisglenn519@yahoo.com>
2020-01-25 11:26:57 -0600

    idk

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
 const mysql = require('mysql')
 
-let connection;
-if(process.env.JAWSDB_URL){
-    connection = mysql.createConnection(process.env.JAWSDB_URL)
-} else {
-    connection = mysql.createConnection({
-        host: 'localhost',
-        port:3306,
-        user:'root',
-        password:'password',
-        database:'burgers_db'
-    })
-}
+let connection =  mysql.createConnection('mysql://stdbqc45lrwi3h0w:rqgacd52el88zt67@ui0tj7jn8pyv9lp6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/nza7gasbdcxr05oq')
+// if(process.env.JAWSDB_URL){
+//     connection = mysql.createConnection(process.env.JAWSDB_URL)
+// } else {
+//     connection = mysql.createConnection({
+//         host: 'localhost',
+//         port:3306,
+//         user:'root',
+//         password:'password',
+//         database:'burgers_db'
+//     })
+// }
 
 connection.connect(function(err){
     if(err) {
