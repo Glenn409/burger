6914d1cb05861049b6c82e1a616f721fbc70d567
Chris Glenn <chrisglenn519@yahoo.com>
2020-01-25 10:46:17 -0600

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql')
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port:3306,
-    user:'root',
-    password:'password',
-    database:'burgers_db'
-})
+const connection = mysql.createConnection('mysql://sft36g6rbqbt885w:q5487vtlh86rzso9@wp433upk59nnhpoh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/g3d8to0e7c0lls8w')
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     port:3306,
+//     user:'root',
+//     password:'password',
+//     database:'burgers_db'
+// })
 
 connection.connect(function(err){
     if(err) {
